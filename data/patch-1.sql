498c280ec30fdb72791c3f11cfe82e64f9aba5ad
iusmani <iusmani@anl.gov>
2020-02-10 16:13:33 -0600

    Update fails on newer versions of mysql or MariaDB if foreign_key_checks is ON.

--- data/patch-1.sql
+++ data/patch-1.sql
@@ -9,9 +9,17 @@ ALTER TABLE name_category MODIFY id VARCHAR(32);
 -- Increase size of column name_category_id in table name_event
 ALTER TABLE name_event MODIFY name_category_id VARCHAR(32);
 
+-- Disable foreign key checks otherwise update fails.
+SET FOREIGN_KEY_CHECKS=0;
+
 -- Make the modifications
+update name_event set name_category_id="component-function" where name_category_id='device-type';
+update name_event set name_category_id="primary-system" where name_category_id='subsystem';
+update name_event set name_category_id="primary-machine-location" where name_category_id='system';
 update name_category set id="component-function", name="Component Function", description="Component Function", version=0 where id='device-type';
 update name_category set id="primary-system", name="Primary System", description="Primary System", version=0 where id='subsystem';
 update name_category set id="primary-machine-location", name="Primary Machine Location", description="Primary Machine Location", version=0 where id='system';
 
+-- Enable foreign key checks again
+SET FOREIGN_KEY_CHECKS=1;
 
